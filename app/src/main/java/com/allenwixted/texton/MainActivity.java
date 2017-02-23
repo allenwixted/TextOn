0489ff3bd5c3b751386d9de6e66ac0cfced604b0
Allen Wixted <allenwixted@gmail.com>
2017-02-23 18:16:40 +0000

    Updated UI spacing.

--- app/src/main/java/com/allenwixted/texton/MainActivity.java
+++ app/src/main/java/com/allenwixted/texton/MainActivity.java
@@ -1,7 +1,10 @@
 package com.allenwixted.texton;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -9,19 +12,25 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText phoneNumber;
-    private EditText smsBody;
     private Button smsManagerBtn;
-    private Button smsSendToBtn;
-    private Button smsViewBtn;
+    private Switch heatSwitch;
+    private Switch boostSwitch;
+    private SeekBar boostSlider;
+    private SharedPreferences sp;
+    private String phoneNumberSP;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,10 +38,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         phoneNumber = (EditText) findViewById(R.id.phoneNumber);
-        smsBody = (EditText) findViewById(R.id.smsBody);
         smsManagerBtn = (Button) findViewById(R.id.smsManager);
-        smsSendToBtn = (Button) findViewById(R.id.smsSIntent);
-        smsViewBtn = (Button) findViewById(R.id.smsVIntent);
+        heatSwitch = (Switch) findViewById(R.id.heatSwitch);
+        boostSwitch = (Switch) findViewById(R.id.boostSwitch);
+        boostSlider = (SeekBar) findViewById(R.id.boostSlider);
+
+        phoneNumber.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    sp.edit().putString("phoneNumber", phoneNumber.getText().toString()).apply();
+                    Log.i("SP", sp.getString("phoneNumber", "ERROR READING"));
+
+                }
+            }
+        });
+
+        sp = this.getSharedPreferences("com.allenwixted.texton", Context.MODE_PRIVATE);
+        //sp.edit().putString("phoneNumber", "Enter your unit's phone number").apply();
+        phoneNumberSP = sp.getString("phoneNumber", "Enter your unit's phone number");
+
+        phoneNumber.setText(phoneNumberSP);
+
+        Log.i("SP", phoneNumberSP);
 
         // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(this,
@@ -52,9 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.READ_SMS}, 1);
 
             } else {
-
                 // No explanation needed, we can request the permission.
-
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.READ_SMS}, 1);
             }
         }
@@ -64,31 +90,18 @@ public class MainActivity extends AppCompatActivity {
                 sendSmsByManager();
             }
         });
-
-        smsSendToBtn.setOnClickListener(new OnClickListener() {
-            public void onClick(View view) {
-                sendSmsBySIntent();
-            }
-        });
-
-        smsViewBtn.setOnClickListener(new OnClickListener() {
-            public void onClick(View view) {
-                sendSmsByVIntent();
-            }
-        });
-
     }
 
     public void sendSmsByManager() {
         try {
             // Get the default instance of the SmsManager
             SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNumber.getText().toString(),
+            smsManager.sendTextMessage(phoneNumberSP,
                     null,
-                    smsBody.getText().toString(),
+                    "SMS CODES WILL GO HERE",
                     null,
                     null);
-            Toast.makeText(getApplicationContext(), "Your sms has successfully sent!",
+            Toast.makeText(getApplicationContext(), "Commands sent to heater successfully",
                     Toast.LENGTH_LONG).show();
         } catch (Exception ex) {
             Toast.makeText(getApplicationContext(),"SMS Failed, Check Credit and #",
@@ -97,37 +110,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void sendSmsBySIntent() {
-        // add the phone number in the data
-        Uri uri = Uri.parse("smsto:" + phoneNumber.getText().toString());
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-        Intent smsSIntent = new Intent(Intent.ACTION_SENDTO, uri);
-        // add the message at the sms_body extra field
-        smsSIntent.putExtra("sms_body", smsBody.getText().toString());
-        try{
-            startActivity(smsSIntent);
-        } catch (Exception ex) {
-            Toast.makeText(MainActivity.this, "Your sms has failed...",
-                    Toast.LENGTH_LONG).show();
-            ex.printStackTrace();
-        }
-    }
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    smsManagerBtn.setEnabled(true);
+                } else {
 
-    public void sendSmsByVIntent() {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    smsManagerBtn.setEnabled(false);
+                }
+                return;
+            }
 
-        Intent smsVIntent = new Intent(Intent.ACTION_VIEW);
-        // prompts only sms-mms clients
-        smsVIntent.setType("vnd.android-dir/mms-sms");
-
-        // extra fields for number and message respectively
-        smsVIntent.putExtra("address", phoneNumber.getText().toString());
-        smsVIntent.putExtra("sms_body", smsBody.getText().toString());
-        try{
-            startActivity(smsVIntent);
-        } catch (Exception ex) {
-            Toast.makeText(MainActivity.this, "Your sms has failed...",
-                    Toast.LENGTH_LONG).show();
-            ex.printStackTrace();
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
 }
